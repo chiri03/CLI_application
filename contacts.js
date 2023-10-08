a58fa617eae7300473a3bf0f3380d1e7959c9f17
chiri03 <lena.kiriac.97@gmail.com>
2023-10-08 20:11:26 +0300

    learn note.js firts lesson

---
+++ contacts.js
@@ -0,0 +1,72 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.table(contacts);
+    })
+    .catch((error) => console.log(error.message));
+}
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const contact = contacts.find((c) => c.id === contactId);
+
+      if (contact) {
+        console.table([contact]);
+      } else {
+        console.log("Contact not found");
+      }
+    })
+    .catch((error) => console.log(error.message));
+}
+function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const newContacts = contacts.filter((c) => c.id !== contactId);
+      return fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    })
+    .then(() => {
+      return fs.readFile(contactsPath);
+    })
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.table(contacts);
+    })
+    .catch((error) => console.log(error.message));
+}
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const newContact = {
+        id: uuidv4(),
+        name,
+        email,
+        phone,
+      };
+      const newContacts = [...contacts, newContact];
+      return fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    })
+    .then(() => {
+      return fs.readFile(contactsPath);
+    })
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.table(contacts);
+    })
+    .catch((error) => console.log(error.message));
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,53 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      if (!id) {
+        throw new Error("You must provide an id");
+      }
+      getContactById(id);
+      break;
+
+    case "add":
+      if (!name || !email || !phone) {
+        throw new Error("You must provide a name, email and phone number");
+      }
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      if (!id) {
+        throw new Error("You must provide an id");
+      }
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
